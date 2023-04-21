7014d9fefaa6940fb49859613bbd84e893b5e590
XitaRps <xitarps@gmail.com>
2023-04-21 20:17:39 -0300

    008 [Test]: Add visit homepage test considering system selenium web driver
    obs:
    ```
    sudo apt-get install chromium-chromedriver
    ```

---
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Visitor visit homepage' do
+  context 'get /' do
+    it 'should return Welcome' do
+      visit '/'
+
+      expect(page).to have_content('Welcome')
+    end
+  end
+end
